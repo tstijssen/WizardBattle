dd374e36f7735b60cdcb81f88cb67073af708224
tstijssen <tstijssen@gmail.com>
2018-10-05 14:34:29 +0100

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public enum ePlayerPriority{PlayerOne, PlayerTwo};
+
+public class GameManager : MonoBehaviour {
+
+	int m_AIGame;
+
+	public PlayerControl m_PlayerOne, m_PlayerTwo;
+	public GameObject m_TurnTorchOne, m_TurnTorchTwo;
+	public GameObject m_Player1Idle, m_Player1Victory, m_Player1Death;
+	public GameObject m_Player2Idle, m_Player2Victory, m_Player2Death;
+	public ePlayerPriority m_Turn;
+	public Text m_TurnText;
+
+	public WizardAI m_AI;
+
+	public AudioClip m_FireSound, m_IceSound, m_VictorySound, m_ButtonSound;
+	private AudioSource m_AudioSource;
+
+	// Use this for initialization
+	void Start () {
+		m_AudioSource = GetComponent<AudioSource> ();
+		m_AIGame = PlayerPrefs.GetInt("AIActive");
+		// roll for player initiative, 0-3 = player 1, 4-6 = player 2
+
+		m_PlayerOne.Setup ();
+		m_PlayerTwo.Setup ();
+
+		int roll = Random.Range (0, 7);
+
+		if (roll > 3) 
+		{
+			m_Turn = ePlayerPriority.PlayerTwo;
+			if (m_AIGame == 0)
+				m_PlayerTwo.DisplayMovement ();
+			else
+				m_AI.CalculateMove ();
+			m_TurnTorchTwo.SetActive (true);
+			m_TurnText.text = "Player 2's Turn";
+		}
+		else 
+		{
+			m_Turn = ePlayerPriority.PlayerOne;
+			m_PlayerOne.DisplayMovement ();
+			m_TurnTorchOne.SetActive (true);
+			m_TurnText.text = "Player 1's Turn";
+		}
+
+	}
+
+	public void DisplaySpellTiles()
+	{
+		if (m_Turn == ePlayerPriority.PlayerOne)
+		{
+			m_PlayerOne.DisplaySpellTiles ();
+		}
+		else if (m_Turn == ePlayerPriority.PlayerTwo && m_AIGame == 0)
+		{
+			m_PlayerTwo.DisplaySpellTiles ();
+		}
+	}
+
+	public void HideSpellTiles()
+	{
+		if (m_Turn == ePlayerPriority.PlayerOne)
+		{
+			m_PlayerOne.HideSpellTiles ();
+		}
+		else if (m_Turn == ePlayerPriority.PlayerTwo)
+		{
+			m_PlayerTwo.HideSpellTiles ();
+		}
+	}
+
+	public void PlayIceSound()
+	{
+		m_AudioSource.PlayOneShot (m_IceSound);
+	}
+
+	public void PlayFireSound()
+	{
+		m_AudioSource.PlayOneShot (m_FireSound);
+	}
+
+	public void PlayButtonSound()
+	{
+		m_AudioSource.PlayOneShot (m_ButtonSound);
+	}
+
+	public void TurnSwitch()
+	{
+		if (m_Turn == ePlayerPriority.PlayerOne) 
+		{
+			m_Turn = ePlayerPriority.PlayerTwo;
+			m_PlayerOne.m_State = WizardState.Casting;
+			m_TurnTorchOne.SetActive (false);
+			m_TurnTorchTwo.SetActive (true);
+			m_TurnText.text = "Player 2's Turn";
+			if (m_AIGame == 0)
+				m_PlayerTwo.DisplayMovement ();
+			else
+				m_AI.CalculateMove ();
+		} 
+		else
+		{
+			m_Turn = ePlayerPriority.PlayerOne;
+			m_PlayerTwo.m_State = WizardState.Casting;
+			m_TurnTorchOne.SetActive (true);
+			m_TurnTorchTwo.SetActive (false);
+			m_TurnText.text = "Player 1's Turn";
+			m_PlayerOne.DisplayMovement ();
+		}
+
+
+	}
+
+	public void GameOver()
+	{
+		m_Player1Idle.SetActive (false);
+		m_Player2Idle.SetActive (false);
+		m_AudioSource.PlayOneShot (m_VictorySound);
+		HideSpellTiles ();
+		// turn over is at start of move, so previous player is the winner
+		if (m_Turn == ePlayerPriority.PlayerTwo) 
+		{
+			// player 1 win
+			m_TurnText.text = "Player 1 Wins!";
+			m_Player1Victory.SetActive (true);
+			m_Player2Death.SetActive (true);
+		}
+		else if (m_Turn == ePlayerPriority.PlayerOne) 
+		{
+			// player 2 win
+			m_TurnText.text = "Player 2 Wins!";
+			m_Player2Victory.SetActive (true);
+			m_Player1Death.SetActive (true);
+		}
+	}
+
+	public void GoToMainMenu()
+	{
+		SceneManager.LoadScene ("Menu");
+	}
+}
---
+++ WizardAI.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct sMovePath
+{
+	public List<TileManager> m_AvailableMoves;
+	public int x, y;
+}
+
+public class WizardAI : MonoBehaviour {
+
+	public PlayerControl m_PlayerControl;
+	public PlayerControl m_Opponent;
+	public GameObject m_IceSpell, m_FireSpell;
+	public float m_Delay;
+
+	IEnumerator DelayAI()
+	{
+		yield return new WaitForSeconds (m_Delay);
+		CalculateSpell ();
+	}
+
+	sMovePath GetPossibleMoves(int x, int y)
+	{
+		sMovePath newPath;
+		newPath.x = x;
+		newPath.y = y;
+		newPath.m_AvailableMoves = new List<TileManager>();
+
+		int movementValue = m_PlayerControl.m_TileMap [y].m_Tiles [x].GetComponent<TileManager>().m_Value;
+		if (x + movementValue < 5 && 
+			!m_PlayerControl.m_TileMap [y].m_Tiles [x + movementValue].GetComponent<TileManager>().m_Player &&
+			m_PlayerControl.m_TileMap [y].m_Tiles [x + movementValue].GetComponent<TileManager>().m_Value < 5 &&
+			m_PlayerControl.m_TileMap [y].m_Tiles [x + movementValue].GetComponent<TileManager>().m_Value > 0) 
+		{
+			newPath.m_AvailableMoves.Add (m_PlayerControl.m_TileMap [y].m_Tiles [x + movementValue].GetComponent<TileManager> ());
+		}
+		if(x - movementValue >= 0 &&
+			!m_PlayerControl.m_TileMap [y].m_Tiles [x - movementValue].GetComponent<TileManager>().m_Player &&
+			m_PlayerControl.m_TileMap [y].m_Tiles [x - movementValue].GetComponent<TileManager>().m_Value < 5 &&
+			m_PlayerControl.m_TileMap [y].m_Tiles [x - movementValue].GetComponent<TileManager>().m_Value > 0) 
+		{
+			newPath.m_AvailableMoves.Add (m_PlayerControl.m_TileMap [y].m_Tiles [x - movementValue].GetComponent<TileManager> ());
+		}
+		if (y + movementValue < 5 &&
+			!m_PlayerControl.m_TileMap [y + movementValue].m_Tiles [x].GetComponent<TileManager>().m_Player &&
+			m_PlayerControl.m_TileMap [y + movementValue].m_Tiles [x].GetComponent<TileManager>().m_Value < 5 &&
+			m_PlayerControl.m_TileMap [y + movementValue].m_Tiles [x].GetComponent<TileManager>().m_Value > 0) 
+		{
+			newPath.m_AvailableMoves.Add (m_PlayerControl.m_TileMap [y + movementValue].m_Tiles [x].GetComponent<TileManager> ());
+		}
+		if(y - movementValue >= 0 &&
+			!m_PlayerControl.m_TileMap [y - movementValue].m_Tiles [x].GetComponent<TileManager>().m_Player &&
+			m_PlayerControl.m_TileMap [y - movementValue].m_Tiles [x].GetComponent<TileManager>().m_Value < 5 &&
+			m_PlayerControl.m_TileMap [y - movementValue].m_Tiles [x].GetComponent<TileManager>().m_Value > 0) 
+		{
+			newPath.m_AvailableMoves.Add (m_PlayerControl.m_TileMap [y - movementValue].m_Tiles [x].GetComponent<TileManager> ());
+		}
+
+		return newPath;
+	}
+
+	public void CalculateMove()
+	{
+		//calculate possible moves
+		sMovePath m_PossibleMoves = GetPossibleMoves(m_PlayerControl.m_CurrentX, m_PlayerControl.m_CurrentY);
+
+		// if only one move left, simply go there
+		if (m_PossibleMoves.m_AvailableMoves.Count == 1)
+			m_PlayerControl.Move ((int)m_PossibleMoves.m_AvailableMoves [0].transform.position.x, (int)m_PossibleMoves.m_AvailableMoves [0].transform.position.y);
+		else if (m_PossibleMoves.m_AvailableMoves.Count == 0) // if no moves left, AI loses game
+		{
+			m_PlayerControl.m_State = WizardState.Dead;
+			m_PlayerControl.m_Manager.GameOver ();
+			return;
+		}
+
+		List<sMovePath> m_MoveList = new List<sMovePath>();
+
+		// check moves after next
+		foreach (TileManager tile in m_PossibleMoves.m_AvailableMoves) 
+		{
+			m_MoveList.Add(GetPossibleMoves((int)tile.transform.position.x, (int)tile.transform.position.y));
+		}
+		
+		m_MoveList.Sort(delegate(sMovePath x, sMovePath y) {
+			return x.m_AvailableMoves.Count - y.m_AvailableMoves.Count;
+		}
+		);
+
+		// move to tile with most available future options
+		m_PlayerControl.Move ((int)m_MoveList [0].x, (int)m_MoveList [0].y);
+
+		StartCoroutine (DelayAI());
+	}
+
+	void CalculateSpell()
+	{
+		// calculate opponents possible moves
+		sMovePath m_PossibleMoves = GetPossibleMoves(m_Opponent.m_CurrentX, m_Opponent.m_CurrentY);
+		TileManager targetTile;
+		// if only one move left and tile is 1 or 4, simply cast spell to kill
+		if (m_PossibleMoves.m_AvailableMoves.Count == 1) 
+		{
+			targetTile = m_PlayerControl.m_TileMap [(int)m_PossibleMoves.m_AvailableMoves [0].transform.position.y].m_Tiles [(int)m_PossibleMoves.m_AvailableMoves [0].transform.position.x].GetComponent<TileManager> ();
+		
+			if (targetTile.m_Value == 1) 
+			{
+				targetTile.m_Manager.PlayIceSound ();
+				m_IceSpell.transform.position = targetTile.transform.position;
+				m_IceSpell.SetActive (true);
+				targetTile.EnchantTile (-1);
+			} 
+			else if (targetTile.m_Value == 4) 
+			{
+				targetTile.m_Manager.PlayFireSound ();
+				m_FireSpell.transform.position = targetTile.transform.position;
+				m_FireSpell.SetActive (true);
+				targetTile.EnchantTile (1);
+			}
+		}
+		else if (m_PossibleMoves.m_AvailableMoves.Count == 0)
+		{
+			m_PlayerControl.m_Manager.TurnSwitch ();
+			return;	// should technically cast a spell before passing turn but whatevs
+		}
+			
+		// otherwise cast spell randomly on one of the opponents next tiles?
+		int randomTile = Random.Range(0, m_PossibleMoves.m_AvailableMoves.Count);
+		targetTile = m_PlayerControl.m_TileMap [(int)m_PossibleMoves.m_AvailableMoves [randomTile].transform.position.y].m_Tiles [(int)m_PossibleMoves.m_AvailableMoves [randomTile].transform.position.x].GetComponent<TileManager> ();
+
+		if (targetTile.m_Value >= 3) 
+		{
+			targetTile.m_Manager.PlayFireSound ();
+			m_FireSpell.transform.position = targetTile.transform.position;
+			m_FireSpell.SetActive (true);
+			targetTile.EnchantTile (1);
+		} 
+		else 
+		{
+			targetTile.m_Manager.PlayIceSound ();
+			m_IceSpell.transform.position = targetTile.transform.position;
+			m_IceSpell.SetActive (true);
+			targetTile.EnchantTile (-1);
+		}
+
+	}
+}
