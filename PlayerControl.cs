871017d01045032a2e6e4bdf7ba6d59de510569f
tstijssen <tstijssen@gmail.com>
2018-10-05 14:33:25 +0100

    Update PlayerControl.cs

--- PlayerControl.cs
+++ PlayerControl.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public enum WizardState{Moving, Casting, Idle, Dead};
 
-
-[System.Serializable]
+// holds references to all tiles on the map in rows and columns
+[System.Serializable]	
 public struct sTileRow
 {
 	public GameObject[] m_Tiles;
@@ -14,14 +14,14 @@ public struct sTileRow
 public class PlayerControl : MonoBehaviour {
 
 	public GameManager m_Manager;
-	public sTileRow[] m_TileMap;
-	public GameObject[] m_MovementIndicators;
-	public TileManager m_CurrentTile;
-	public bool m_Dead;
+	public sTileRow[] m_TileMap;	// game map 5x5 grid
+	public GameObject[] m_MovementIndicators;	// sprites to indicate where player can move
+	public TileManager m_CurrentTile;		// reference to current tile player is on
 	public WizardState m_State;
-	public int m_CurrentX, m_CurrentY;
+	public int m_CurrentX, m_CurrentY;		// current grid position
 
-	public SpriteAnimator m_IdleAnimation;
+	// animations for idling, casting, moving and dying states
+	public SpriteAnimator m_IdleAnimation;		
 	public SpriteAnimator m_CastingAnimation;
 	public SpriteAnimator m_MovingAnimation;
 	public SpriteAnimator m_DeathAnimation;
@@ -31,25 +31,31 @@ public class PlayerControl : MonoBehaviour {
 
 	public void Move(int x, int y)
 	{
+		// grab reference to next tile
 		TileManager nextTile = m_TileMap [y].m_Tiles[x].GetComponent<TileManager> ();
 
+		// previous tile no longer contains a player
 		m_CurrentTile.m_Player = false;
 
+		// move to new square
 		this.transform.position = nextTile.transform.position;
 		nextTile.m_Player = true;
 		m_CurrentTile = nextTile;
 		m_CurrentX = x;
 		m_CurrentY = y;
-		m_State = WizardState.Moving;
+		m_State = WizardState.Moving; // trigger animation
 
 		m_AudioPlayer.PlayOneShot (m_MoveSound);
-		DisplaySpellTiles ();
+		DisplaySpellTiles (); // go to next stage
 	}
 
+	// display valid movements
 	public void DisplayMovement()
 	{
 		HideSpellTiles ();
-		int movementValue = m_CurrentTile.m_Value;
+		int movementValue = m_CurrentTile.m_Value; // get movement value for turn
+		
+		// check wether movement is within boundaries, and whether next tile in all 4 directions is valid
 		if (m_CurrentX + movementValue < 5 && 
 			!m_TileMap [m_CurrentY].m_Tiles [m_CurrentX + movementValue].GetComponent<TileManager>().m_Player &&
 			m_TileMap [m_CurrentY].m_Tiles [m_CurrentX + movementValue].GetComponent<TileManager>().m_Value < 5 &&
@@ -83,17 +89,20 @@ public class PlayerControl : MonoBehaviour {
 			m_MovementIndicators [3].gameObject.SetActive (true);
 		}
 
+		// if any movements are valid, return
 		foreach (GameObject movement in m_MovementIndicators) 
 		{
 			if (movement.activeInHierarchy)
 				return;
 		}
 
+		// if no movements are valid, game over for player
 		m_AudioPlayer.PlayOneShot (m_DeathSound);
 		m_State = WizardState.Dead;
 		m_Manager.GameOver ();
 	}
 
+	// hide indicators for casting spells on tiles
 	public void HideSpellTiles()
 	{
 		for (int row = 0; row < m_TileMap.Length; ++row) 
@@ -108,6 +117,7 @@ public class PlayerControl : MonoBehaviour {
 		}
 	}
 
+	// show indicators for casting spells on tiles
 	public void DisplaySpellTiles()
 	{
 		foreach (GameObject mov in m_MovementIndicators) {
@@ -140,6 +150,7 @@ public class PlayerControl : MonoBehaviour {
 		switch (m_State) 
 		{
 		case WizardState.Idle:
+				// idle animation loops
 			m_IdleAnimation.enabled = true;
 			m_DeathAnimation.enabled = false;
 			m_MovingAnimation.enabled = false;
@@ -152,6 +163,7 @@ public class PlayerControl : MonoBehaviour {
 			m_MovingAnimation.enabled = false;
 			m_IdleAnimation.enabled = false;
 
+			// casting animation reverts to idle when done
 			if (m_CastingAnimation.m_AnimationFinished) 
 			{
 				m_State = WizardState.Idle;
@@ -165,11 +177,13 @@ public class PlayerControl : MonoBehaviour {
 			m_IdleAnimation.enabled = false;
 			m_CastingAnimation.enabled = false;
 
+			// moving animation reverts to idle when done
 			if (m_MovingAnimation.m_AnimationFinished)
 				m_State = WizardState.Idle;
 			break;
 
 		case WizardState.Dead:
+				// death animation stops after one loop
 			m_DeathAnimation.enabled = true;
 			m_MovingAnimation.enabled = false;
 			m_IdleAnimation.enabled = false;
